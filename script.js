6a191b53d4c8d76fcb60087b5200f73c3cbf0fa2
Luiz F. Picolo <thaissa2608mariana@gmail.com>
2023-06-14 13:51:28 -0400

    Commit alte

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 //divlista-produtos.insertAdjacentHTML('afterbegin',  mostrar_Produto_Destaque());
+class MeuErro extends Error {
+    constructor(message){
+      super(message);
+      this.name = "Meu Erro";
+    }
+  }
+
 class Produto {
     constructor(nome, data_do_cadastro, descricao_do_produto, preco){
         this.nome = nome;
@@ -8,6 +15,7 @@ class Produto {
     }
 
     mostrar_dados(){
+        if (this.nome != ""){
         return `
         <div class="produto-cardd">
         <div class="rosa">${this.nome}</div>
@@ -16,9 +24,13 @@ class Produto {
         <div class="rosa">${this.preco}</div>
         </div>
         `;
+    } else {
+        throw new MeuErro("Mensagem de erro");
+      }
+    }
         //return this.nome +  this.data_do_cadastro + this.descricao_do_produto + this.preco;
     }
-}
+
 
 class Produto_Destaque extends Produto{
         constructor(nome, data_do_cadastro , descricao_do_produto, preco, produto_destaque){
@@ -41,12 +53,20 @@ class Produto_Destaque extends Produto{
         
 }
 
+mostrarAtributos() {
+    try {
+      return this.atributos();
+    } catch (error) {
+      return error
+    }
+  }
+
 const produtos = new Produto_Destaque("Sanduicheira Hello Kitty", "26/02/2023", "1234", "410", "https://ae01.alicdn.com/kf/HTB1XYcyHVXXXXa4XpXXq6xXFXXXM/Hellokitty-sandwich-maker-bread-maker-Toaster-2-min-make-a-sandwich-or-bread-breakfast-maker-cooking.jpg_220x220.jpg_.webp");
 console.log(produtos.mostrar_Produto_Destaque());
 const div = document.getElementById('produto_destaque');
 div.insertAdjacentHTML('afterbegin', produtos.mostrar_Produto_Destaque());
 
-const produtoss = new Produto_Destaque("Câmera Hello Kitty", "30/04/2023", "1067", "420");
+const produtoss = new Produto("Câmera Hello Kitty", "30/04/2023", "1067", "420");
 console.log(produtoss.mostrar_dados());
 const divv = document.getElementById('lista_produtos');
 divv.insertAdjacentHTML('afterbegin', produtoss.mostrar_dados());
@@ -56,7 +76,7 @@ console.log(produtos2.mostrar_Produto_Destaque());
 const div2 = document.getElementById('produto_destaque');
 div2.insertAdjacentHTML('afterbegin', produtos2.mostrar_Produto_Destaque());
 
-const produtos22 = new Produto_Destaque("Luminária Hello Kitty", "13/03/2023", "1134", "130");
+const produtos22 = new Produto("Luminária Hello Kitty", "13/03/2023", "1134", "130");
 console.log(produtos22.mostrar_dados());
 const div22 = document.getElementById('lista_produtos');
 div22.insertAdjacentHTML('afterbegin', produtos22.mostrar_dados());
@@ -66,7 +86,7 @@ console.log(produtos3.mostrar_Produto_Destaque());
 const div3 = document.getElementById('produto_destaque');
 div3.insertAdjacentHTML('afterbegin', produtos3.mostrar_Produto_Destaque());
 
-const produtos33 = new Produto_Destaque("Cafeteira Hello Kitty", "16/01/2023", "1289", "560");
+const produtos33 = new Produto("Cafeteira Hello Kitty", "16/01/2023", "1289", "560");
 console.log(produtos33.mostrar_dados());
 const div33 = document.getElementById('lista_produtos');
 div33.insertAdjacentHTML('afterbegin', produtos33.mostrar_dados());
@@ -76,7 +96,7 @@ console.log(produtos4.mostrar_Produto_Destaque());
 const div4 = document.getElementById('produto_destaque');
 div4.insertAdjacentHTML('afterbegin', produtos4.mostrar_Produto_Destaque())
 
-const produtos44 = new Produto_Destaque("Tablet Hello Kitty", "15/04/2023", "1550", "1000");
+const produtos44 = new Produto("Tablet Hello Kitty", "15/04/2023", "1550", "1000");
 console.log(produtos44.mostrar_dados());
 const div44 = document.getElementById('lista_produtos');
 div44.insertAdjacentHTML('afterbegin', produtos44.mostrar_dados());
@@ -86,7 +106,7 @@ console.log(produtos5.mostrar_Produto_Destaque());
 const div5 = document.getElementById('produto_destaque');
 div5.insertAdjacentHTML('afterbegin', produtos5.mostrar_Produto_Destaque());
 
-const produtos55 = new Produto_Destaque("Notebook Hello Kitty", "12/03/2023", "1330", "1450");
+const produtos55 = new Produto("Notebook Hello Kitty", "12/03/2023", "1330", "1450");
 console.log(produtos55.mostrar_dados());
 const div55 = document.getElementById('lista_produtos');
 div55.insertAdjacentHTML('afterbegin', produtos55.mostrar_dados());
